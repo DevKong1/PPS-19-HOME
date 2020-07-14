f7e0651b5994ed9f971b86e3eb4b5a562e7b068d
Manuele Pasini <pasottipro@gmail.com>
2020-07-14 17:18:29 +0200

    Switched to scala-swing

--- src/main/scala/HOME/GUI.scala
+++ src/main/scala/HOME/GUI.scala
@@ -1,32 +1,13 @@
-import javafx.application.Application
-import javafx.geometry.Pos
-import javafx.scene.Scene
-import javafx.scene.layout.{HBox, StackPane, VBox}
-import javafx.stage.Stage
-import javafx.scene.control.{Button, Label}
+package HOME
 
-class GUI extends Application {
-  println("GUI()")
+import scala.swing._
 
-  override def start(primaryStage: Stage) {
-    primaryStage.setTitle("HOME")
-
-    val root = new VBox()
-    val title = new HBox(new Label("Welcome to your HOME!"))
-    title setAlignment Pos.CENTER
-
-    val btn = new Button("Join")
-    root.getChildren.add(title)
-    root.getChildren.add(new Label("Hello world!"))
-    root.getChildren.add(btn)
-
-    primaryStage.setScene(new Scene(root, 300, 300))
-    primaryStage.show()
+object SimpleGUI extends SimpleSwingApplication {
+  def top = new MainFrame {
+    title = "Hello, World!"
+    contents = new Button {
+      text = "Click Me!"
+    }
   }
-}
 
-object GUI {
-  def main(args: Array[String]) {
-    Application.launch(classOf[GUI], args: _*)
-  }
 }
