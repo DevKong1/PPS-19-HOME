a9a37b7f796c17bd59727407736b5c924971b6b4
Manuele Pasini <pasottipro@gmail.com>
2020-07-15 13:37:38 +0200

    Refactored GUI, now has main window the correct size

--- src/main/scala/HOME/GUI.scala
+++ src/main/scala/HOME/GUI.scala
@@ -1,36 +1,51 @@
 package HOME
 
 
-import java.awt.{Dimension,GraphicsEnvironment}
+import java.awt.{Dimension, GraphicsEnvironment}
 
+import scala.collection.mutable.ListBuffer
 import scala.swing._
+import scala.swing.event.{MouseClicked, SelectionChanged}
+
+//TODO: RECONSIDER HOW TO MODEL GUIROOM
+class GUIRoom extends Panel with Room{
+  override def devices: ListBuffer[Device] = ???
+}
+
+sealed trait Room{
+  def devices : ListBuffer[Device]
+}
 
 object SimpleGUI extends SimpleSwingApplication {
-  private val FRAMEWIDTH_PROP = 0.55
-  private val FRAMEHEIGHT_PROP = 0.55
-  private val MAX_HEIGHT = ScreenSize("height") * FRAMEHEIGHT_PROP
-  private val MAX_WIDHT = ScreenSize("width") * FRAMEWIDTH_PROP
+
 
   def top = new MainFrame {
     title = "Home!"
-
     val tp = new TabbedPane {
-      pages += new TabbedPane.Page("Home", new ScrollPane())
-      pages += new TabbedPane.Page("Kitchen", new ScrollPane())
+      pages += new TabbedPane.Page("HOME", new GUIRoom())
+      pages += new TabbedPane.Page("+", new GUIRoom())
+      reactions += {
+        ???
+      }
     }
-
+    listenTo(tp.selection)
+    //used to set items in the main window inside a vertical BoxPanel
     contents = new BoxPanel(Orientation.Vertical) {
       contents ++= Seq(tp)
     }
-    size =  new Dimension(MAX_WIDHT toInt,MAX_HEIGHT toInt)
+    size =  WindowSize()
   }
 }
 
-object ScreenSize {
+object WindowSize {
   val SCREEN = GraphicsEnvironment.getLocalGraphicsEnvironment.getMaximumWindowBounds
-  def apply(msg : String): Int = msg match{
-    case "height" => SCREEN height
-    case "width" => SCREEN width
+  private val FRAME_PROP = 0.55
+
+  val height = SCREEN.height * FRAME_PROP toInt
+  val width = SCREEN.width * FRAME_PROP toInt
+
+  def apply(): Dimension = {
+    new Dimension(width ,height)
   }
 
 }
