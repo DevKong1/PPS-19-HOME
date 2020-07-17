3fb7d4bec42034a59ca09b6d746c4402c5bbc4c2
Manuele Pasini <pasottipro@gmail.com>
2020-07-17 15:15:23 +0200

    added "AddDevice" button and architecture for adding a device

--- src/main/scala/HOME/GUI.scala
+++ src/main/scala/HOME/GUI.scala
@@ -2,33 +2,42 @@ package HOME
 
 
 import java.awt.{Dimension, GraphicsEnvironment}
-import java.util.concurrent.Future
-
-import javax.swing.Icon
-
-import scala.collection.GenSet
-import scala.collection.mutable.ListBuffer
-import scala.swing.Dialog.{Message, showInput}
-import scala.swing.Swing.EmptyIcon
 import scala.swing._
-import scala.swing.event.{ButtonClicked, SelectionChanged}
+import scala.swing.event.SelectionChanged
 
 sealed trait Room {
-  def devices : ListBuffer[Device]
+  def devices : Set[Device]
   def name : String
 }
 
-//TODO: RECONSIDER HOW TO MODEL GUIROOM
- class GUIRoom(override val name:String) extends Panel with Room{
-  override def devices: ListBuffer[Device] = ???
+class GUIDevice() extends BorderPanel{
+  ???
+}
+object newDevice {
 
+}
+//TODO: RECONSIDER HOW TO MODEL GUIROOM
+ class GUIRoom(override val name:String) extends BoxPanel(Orientation.Vertical) with Room {
+  override def devices: Set[Device] = ???
+  private val x : Dimension = WindowSize()
+
+  val button1 = new Button("Add device"){
+    addDevice()
+  }
+  contents+=  new BorderPanel {
+    add(button1, BorderPanel.Position.South)
+  }
+
+  def addDevice(): Unit = {
+
+  }
   /* var a : Device = new SimulatedLight("Lamp1", name, LightType, 1)
   var b : Device = new SimulatedLight("Lamp2", name, LightType, 1)
   devices += a
   devices += b*/
 
   override def equals(that: Any): Boolean = that match{
-    case that: GUIRoom=> name equals that.name
+    case that: GUIRoom=> this.name equals that.name
     case _ => false
   }
  /* for( c <- devices) {
@@ -36,12 +45,14 @@ sealed trait Room {
     _contents += Swing.HStrut(20)
   }*/
    def apply(roomName: String): GUIRoom =  new GUIRoom(roomName)
+    object deviceAddForm {
+
+    }
 }
 
-object SimpleGUI extends SimpleSwingApplication {
+object GUI extends SimpleSwingApplication {
   private val ADD = "+"
-
-  private var rooms: Set[GUIRoom] = Set(new GUIRoom("Home"), new GUIRoom("Kitchen"), new GUIRoom("Bedroom"), new GUIRoom(ADD))
+  var rooms: Set[GUIRoom] = Set(new GUIRoom("Home"), new GUIRoom("Kitchen"), new GUIRoom("Bedroom"),new GUIRoom(ADD))
 
   val tp: TabbedPane = new TabbedPane {
     for(i <- rooms) yield {pages += new TabbedPane.Page(i.name,i)}
@@ -50,16 +61,17 @@ object SimpleGUI extends SimpleSwingApplication {
       title = "Home!"
       println("Welcome")
       reactions += {
-        case SelectionChanged(_) => {
+        case SelectionChanged(_) =>
           for {
             isLast <- checkLast()
             name <-  getRoomName
           } yield {
             tp.pages.remove(tp.pages.length-1)
+            rooms+= new GUIRoom(name)
             tp.pages+=new TabbedPane.Page(name, new GUIRoom(name))
-            tp.pages+=new TabbedPane.Page(ADD, new GUIRoom(ADD))
+            tp.pages+=new TabbedPane.Page(ADD,new GUIRoom(ADD))
+            //TODO: Merge with controller controller.addRoom(name)
           }
-        }
       }
       //used to set items in the main window inside a vertical BoxPanel
       contents = new BoxPanel(Orientation.Vertical) {
@@ -72,16 +84,13 @@ object SimpleGUI extends SimpleSwingApplication {
       object checkLast {
         def apply(): Option[Boolean] = {
           tp.selection.page.title match {
-            case "+" => {
-              Some(true)
-            }
+            case "+" => Some(true)
             case _ => None
           }
         }
       }
 
-      def getRoomName: Option[String] = {
-        // TODO: SE AGGIUNTA STANZA VIENE CHIUSA DA ANNULLA O X QUALUNQUE SIA IL NOME SPARA ERRORE
+      private def getRoomName: Option[String] = {
         import Dialog._
         val name = showInput(tp,
           "Room name:",
@@ -90,15 +99,17 @@ object SimpleGUI extends SimpleSwingApplication {
           Swing.EmptyIcon,
           Nil, "")
         println(name.isDefined)
-        if (name.isDefined && !name.get.equals(ADD) && !tp.pages.contains(new TabbedPane.Page(name.get, new GUIRoom(name.get)))) name else {
-          showMessage(tp, "Room already existing or incorrect room name", Message.Error toString)
+        //TODO: THINK OF A MORE FUNCTIONAL WAY TO IMPLEMENT INPUT CHECK
+        if (name.isDefined && name.get.trim.length>0 && !name.get.equals(ADD) && !tp.pages.contains(new TabbedPane.Page(name.get, new GUIRoom(name.get)))) name else {
+          if(name.isDefined) {
+            showMessage(tp, "Room already existing or incorrect room name", Message.Error toString)
+          }
           None
         }
 
       }
     }
   }
-
   object WindowSize {
     private val SCREEN = GraphicsEnvironment.getLocalGraphicsEnvironment.getMaximumWindowBounds
     private val FRAME_PROP = 0.55
@@ -109,6 +120,7 @@ object SimpleGUI extends SimpleSwingApplication {
     def apply(): Dimension = {
       new Dimension(width, height)
     }
+
   }
 
 
