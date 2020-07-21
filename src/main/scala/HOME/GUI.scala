34ce1d030a95beef0d1b8f5396b429244a6ac454
Manuele Pasini <pasottipro@gmail.com>
2020-07-21 13:17:19 +0200

    Now you can add devices to rooms
    Still need to add Device to coordinator.

--- src/main/scala/HOME/GUI.scala
+++ src/main/scala/HOME/GUI.scala
@@ -2,123 +2,99 @@ package HOME
 
 import scala.language.postfixOps
 import java.awt.{Dimension, GraphicsEnvironment}
-import scala.collection.mutable.ListBuffer
 import scala.swing._
 import scala.swing.event.{ButtonClicked, SelectionChanged}
+import HOME.MyClass._
 
 sealed trait Room {
   def devices : Set[Device]
   def name : String
 }
 
-class GUIDevice() extends BorderPanel{
-  ???
-}
-object newDevice {
 
-}
 
-//TODO: RECONSIDER HOW TO MODEL GUIROOM
 class GUIRoom(override val name:String) extends BoxPanel(Orientation.Vertical) with Room {
-  override def devices: Set[Device] = ???
-  //private val x : Dimension = WindowSize()
-
-  val buttonDimension = new Dimension(300,70)
-  var tryListDevice = new ListBuffer[Device]()
-  /*if(name != "+") {
-    var a: Device = Light("Lamp1", name)
-    var b: Device = Light("Lamp2", name)
-    tryListDevice += a
-    tryListDevice += b
-  }*/
-
-  /*val button1 = new Button("Add device") {
-    addDevice()
-  }*/
-
-  _contents += new BoxPanel(Orientation.Vertical) {
-    for( c <- tryListDevice) {
-      contents += addButton(c.name)
+   /*always present device in every room*/
+   val light: SimulatedLight = Light("A",name)
+   val AC: SimulatedAirConditioner = AirConditioner("B",name)
+   val dehumidifier: SimulatedDehumidifier = Dehumidifier("C",name)
+   override def devices: Set[Device] = Set(light,AC,dehumidifier)
+    /* lets users add devices inside a room*/
+   private val adDeviceBtn: Button = new Button("Add device") {
+    reactions += {
+      case ButtonClicked(_) =>  DeviceDialog()
     }
-    contents += new BorderPanel {
-      add(addButton("Add device"), BorderPanel.Position.South)
-    }
-  }
+   }
+   val devicePanel = new BoxPanel(Orientation.Vertical)
 
-  def addDevice(): Unit = {
-    DeviceDialog()
-  }
+   val bp: BorderPanel = new BorderPanel {
+      add(adDeviceBtn, BorderPanel.Position.South)
+   }
+  contents += devicePanel
+  contents += bp
+   //Prints every device in this room, starting from the basic 3
+   for (i <- devices)yield{printDevice(i)}
+
+
+
+
+  def apply(roomName: String): GUIRoom = new GUIRoom(roomName)
 
   override def equals(that: Any): Boolean = that match{
     case that: GUIRoom=> this.name equals that.name
     case _ => false
   }
-
-  def apply(roomName: String): GUIRoom =  new GUIRoom(roomName)
-    object deviceAddForm {
-
-    }
-
-  def addButton(buttonName : String) : Button = buttonName match {
-    case buttonName => new Button(buttonName) {
-      minimumSize = buttonDimension
-      maximumSize = buttonDimension
-      preferredSize = buttonDimension
-      name = buttonName
-
-      reactions += {
-        if(name == "Add device") {
-          case ButtonClicked(b) =>
-            addDevice()
-            println("Click me: " + b.name)
-        } else {
-          case ButtonClicked(b) =>
-            println("Ciao")
+  //First release unpretty print
+  object printDevice {
+    def apply(d: Device): Unit = {
+      devicePanel.contents += new Label() {
+        text = {
+          "Device name: " + d.name + "\t" + " Device Type: " + d.getSimpleClassName + "\n" + " Consumption: " + d.consumption + " Status: " + d.isOn
         }
       }
+      repaint()
     }
   }
 }
 
 object GUI extends SimpleSwingApplication {
   private val ADD = "+"
-  var rooms: Set[GUIRoom] = Set(new GUIRoom("Home"), new GUIRoom("Kitchen"), new GUIRoom("Bedroom"),new GUIRoom(ADD))
-
+  var rooms: Set[GUIRoom] = Set(new GUIRoom("Home"), new GUIRoom("Kitchen"), new GUIRoom("Bedroom"))
   val tp: TabbedPane = new TabbedPane {
     for(i <- rooms) yield {pages += new TabbedPane.Page(i.name,i)}
+    pages+= new TabbedPane.Page(ADD,new BorderPanel())
   }
+
     def top: MainFrame = new MainFrame {
       title = "Home!"
       println("Welcome")
       reactions += {
         case SelectionChanged(_) =>
           for {
-            isLast <- checkLast()
-            name <-  getRoomName
+            last <- getLastIndex()
+            name <- getRoomName
           } yield {
-            tp.pages.remove(tp.pages.length-1)
-            rooms+= new GUIRoom(name)
-            tp.pages+=new TabbedPane.Page(name, new GUIRoom(name))
-            tp.pages+=new TabbedPane.Page(ADD,new GUIRoom(ADD))
-            //TODO: Merge with controller controller.addRoom(name)
+            val newRoom = new GUIRoom(name)
+            val newRoomPane = new TabbedPane.Page(name,  newRoom)
+            rooms += newRoom
+            tp.selection.page = newRoomPane
+            tp.pages.insert(last.index,newRoomPane)
           }
       }
       //used to set items in the main window inside a vertical BoxPanel
       contents = new BoxPanel(Orientation.Vertical) {
-        //contents ++= Seq(tp)
         contents += tp
-
       }
       listenTo(tp.selection)
-      size = WindowSize()
+      size = WindowSize(WindowSizeType.MainW)
 
-      object checkLast {
-        def apply(): Option[Boolean] = {
-          tp.selection.page.title match {
-            case "+" => Some(true)
-            case _ => None
-          }
+      object getLastIndex {
+        def apply(): Option[TabbedPane.Page] = {
+          if(tp.selection.page.title equals ADD) {
+            tp.pages.find(page => page.title equals ADD )
+          }else None
         }
+
       }
 
       private def getRoomName: Option[String] = {
@@ -129,13 +105,12 @@ object GUI extends SimpleSwingApplication {
           Message.Plain,
           Swing.EmptyIcon,
           Nil, "")
-        //println(name.isDefined)
         //TODO: THINK OF A MORE FUNCTIONAL WAY TO IMPLEMENT INPUT CHECK
-        if (name.isDefined && name.get.trim.length>0 && !name.get.equals(ADD) && !tp.pages.contains(new TabbedPane.Page(name.get, new GUIRoom(name.get)))) {
+        if (name.isDefined && name.get.trim.length > 0 && !name.get.equals(ADD)&& !tp.pages.exists(page => page.title equals name.get)) {
           Rooms.addRoom(name.get)
           name
         } else {
-          if(name.isDefined) {
+          if (name.isDefined) {
             showMessage(tp, "Room already existing or incorrect room name", Message.Error toString)
           }
           None
@@ -145,14 +120,78 @@ object GUI extends SimpleSwingApplication {
   }
 
 object WindowSize {
+  import WindowSizeType._
   private val SCREEN = GraphicsEnvironment.getLocalGraphicsEnvironment.getMaximumWindowBounds
   private val FRAME_PROP = 0.55
+  private val WIN_PROP_H = 0.2
+  private val WIN_PROP_W = 0.5
 
   val height: Int = SCREEN.height * FRAME_PROP toInt
   val width: Int = SCREEN.width * FRAME_PROP toInt
 
-  def apply(): Dimension = {
-    new Dimension(width, height)
+  def apply(windType : WindowSizeType.Value): Dimension = windType match{
+    case MainW => new Dimension(width, height)
+    case Dialog => new Dimension(width*WIN_PROP_W toInt,height*WIN_PROP_H toInt)
+  }
+}
+object WindowSizeType extends Enumeration {
+  type Type = Value
+  val MainW, Dialog = Value
+}
+
+class CustomDeviceDialog extends Dialog {
+  private val dimension = WindowSize(WindowSizeType.Dialog)
+  private val deviceType = new ComboBox[DeviceType](DeviceType.listTypes toSeq)
+  private val deviceName = new TextField(10)
+  preferredSize = dimension
+  title = "Add device"
+  contents = new BoxPanel(Orientation.Vertical) {
+    private val labels = new FlowPanel() {
+      contents ++= Seq(new Label("Device name: "),deviceName,new Label("Device type: "),deviceType)
+    }
+    private val buttons = new FlowPanel() {
+      contents ++=Seq(
+        new Button("Create"){
+          reactions += {
+            case ButtonClicked(_) =>
+              val name = deviceName.text
+              val currentRoom : String = GUI.tp.selection.page.title
+              val devType = deviceType.selection.item.toString
+              if(name.trim.length > 0){
+                for {
+                  i <- GUI.rooms.find(_.name equals currentRoom)
+                  c <- stringToDevice(devType,name,currentRoom)
+                }yield{
+                  i.printDevice(c)
+                  //CoordinatorImpl().addDevice(c)
+                  close()
+                }
+              }
+          }
+        },
+        new Button("Cancel") {
+        reactions += {
+          case ButtonClicked(_) =>
+            close()
+        }
+      })
+    }
+    contents++= Seq(labels,buttons)
+  }
+  open()
+  def stringToDevice(devType: String,name:String,room : String) : Option[Device] = devType match{
+    case "LightType" => Some(Light(name,room))
+    case "AirConditionerType" => Some(AirConditioner(name,room))
+    case "DehumidifierType" => Some(Dehumidifier(name,room))
+    case "ShutterType" => Some(Shutter(name,room))
+    case "BoilerType" => Some(Boiler(name,room))
+    case "TvType" => Some(TV(name,room))
+    case "WashingMachine" => Some(WashingMachine(name,room))
+    case "DishWasherType" => Some(DishWasher(name,room))
+    case "OvenType" =>Some(Oven(name,room))
+    case "StereoSystem" =>Some( StereoSystem(name,room))
+    case _ => None
+
   }
 }
 
@@ -161,35 +200,4 @@ object DeviceDialog {
     new CustomDeviceDialog()
   }
 }
-
-class CustomDeviceDialog extends Dialog {
-  private val dimension = new Dimension(400, 130)
-
-  title = "Add device"
-  minimumSize = dimension
-  maximumSize = dimension
-  preferredSize = dimension
-  contents = new BoxPanel(Orientation.Vertical) {
-    contents += new BoxPanel(Orientation.Horizontal) {
-      preferredSize = new Dimension(100,45)
-      contents += new Label("Device name: ")
-      contents += new TextField()
-      contents += new Label("Device type: ")
-      contents += new ComboBox[Device](new ListBuffer[Device])
-    }
-    contents += new BoxPanel(Orientation.Horizontal) {
-      preferredSize = new Dimension(100,45)
-      contents += new Button("Cancel") {
-        reactions += {
-          case ButtonClicked(b) =>
-            close()
-        }
-      }
-      contents += new Button("Create")
-    }
-  }
-  open()
-}
-
-
 
